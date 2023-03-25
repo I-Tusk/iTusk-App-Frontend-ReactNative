7e28b22e707876370aa745b681a047cd211dedfb
zhaikul <124695049+zhaikul@users.noreply.github.com>
2023-03-25 11:57:19 +0000

    EmergencyScreen changes

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './app/screens/HomeScreen';
+import EmergencyScreen from './app/screens/EmergencyScreen/EmergencyScreen';
 
 export default function App() {
   return (
-    <HomeScreen/>
+    <EmergencyScreen/>
   );
 }
 
